b1479f407f9c2d881bf0f5cb58ef31cd8c6f5c24
buurjoie <seydinamandionem9@gmail.com>
2023-03-09 23:53:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import Profile from "./profill/profile";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 
 function App() {
+function handleName(name){return alert(name);};
+let fullName="Seydina Mandione mbaye";
+let bio="Developpeur Full Stack et aussi etudiant en informatique de developpement d'application web mobile a l'Universite Virtuelle du Senegal et a Gomycode. J'ai 24ans et je suis en 2e annee de licence.";
+let profession="Developpeur Full Stack";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="body">
+    <div className="container">
+    <Profile fullName={fullName} bio={bio} profession={profession} handleName={handleName}/> 
     </div>
+    </div>
+   </>
   );
 }
 
 export default App;
+
+
---
+++ src/profill/profile.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import propTypes from 'prop-types'; 
+import image1 from "./profile.JPG";
+import image2 from "./photo.jpeg";
+function Profile({fullName ='Le nom de Profile', bio = 'biografie', profession="profession",handleName}) {
+  handleName(fullName);
+  const stylename = {color:"rgb(120, 48, 183)",fontWeight: "bolder",fontSize: "30px",};
+const styleprfs={ color:'rgb(218, 255, 11)',fontWeight:'bolder'};
+const divestyle={  paddingTop: "10px",paddingLeft:"5px"};
+const block1 = {  display: "flex", flexDirection: "row",  paddingTop: "30px"};
+const block2 = {  display: "flex",flexDirection: "column",justifyContent: "center"};
+const img2 = { minWidth: "70px"};
+const img1 = { width: "18vmin", borderRadius: "10px"};
+  return(
+    <>
+   <div className="row">
+      <div className="col col-md-12" style={block1}>
+        <img src={image1} style={img1} alt="img" />
+      <div style={divestyle}>   
+      <h1 style={stylename}>{fullName}</h1>
+      <h3 style={styleprfs}>{profession}</h3>
+      </div>
+      </div>
+      <div className='col-md-12' style={block2}>
+      <h4>{bio}</h4>
+      <img src={image2} style={img2}  alt="img2" />
+      </div>
+  </div>
+  </>
+  );
+ };
+Profile.propTypes={
+    fullName:propTypes.string,
+    bio:propTypes.string,
+    profession:propTypes.string,
+    handlename:propTypes.func,
+};
+export default Profile;
